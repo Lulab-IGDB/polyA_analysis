b60f1a475f44aff8bca12ca6e0b97fe005434265
Lulab-IGDB <90339246+Lulab-IGDB@users.noreply.github.com>
2022-01-17 21:52:39 +0800

    Update extract_and_filter_for_nonUMI_samples.sh

--- PAIso-seq/extract_and_filter_for_nonUMI_samples.sh
+++ PAIso-seq/extract_and_filter_for_nonUMI_samples.sh
@@ -1,10 +1,10 @@
 # bash extract_and_filter.sh sample_name
-fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/mouse/chr_pc.txt - >$1.chr_pc.polyA_note.pass1.txt
-fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/mouse/chr_lnc.txt - >$1.chr_lnc.polyA_note.pass1.txt
-fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/mouse/mt_pc.txt - >$1.mt_pc.polyA_note.pass1.txt
-fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/mouse/mt_rRNA.txt - >$1.mt_rRNA.polyA_note.pass1.txt
-fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/mouse/hist.txt - >$1.hist.polyA_note.pass1.txt
-fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/mouse/hist_var.txt - >$1.hist_var.polyA_note.pass1.txt
+fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/human/chr_pc.txt - >$1.chr_pc.polyA_note.pass1.txt
+fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/human/chr_lnc.txt - >$1.chr_lnc.polyA_note.pass1.txt
+fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/human/mt_pc.txt - >$1.mt_pc.polyA_note.pass1.txt
+fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/human/mt_rRNA.txt - >$1.mt_rRNA.polyA_note.pass1.txt
+fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/human/hist.txt - >$1.hist.polyA_note.pass1.txt
+fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | fisher.pl -fc 3 ./annotations/human/hist_var.txt - >$1.hist_var.polyA_note.pass1.txt
 fisher.pl -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt >$1.chr_rRNA.polyA_note.pass1.txt
 fisher.pl --except -fc 2 $1.chr_rRNA_reads.txt $1.polyA_note.txt | awk '$3 == "Unknown"' >$1.unknown.polyA_note.pass1.txt
 
